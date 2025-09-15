984855534835b7413a07109667c7cb6b37337238
EssieDyva <st3f1703@gmail.com>
2025-09-15 15:33:47 +0200

    small changes

---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <nav class="navbar">
+            <div class="logo">
+                <img src="/vite.svg" alt="Vite Logo" />
+                <img src="./assets/vue.svg" alt="Vue Logo" />
+            </div>
+            <ul class="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/Test.vue
+++
@@ -1,1 +0,0 @@
-ygf8sgetrgvbvgfdy
