22904a1bea43e3d3d27dc0c8d86ca4a0235f7ecc
EssieDyva <st3f1703@gmail.com>
2025-09-15 15:50:18 +0200

    files ready

---
+++ src/views/Header.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<template>
+    <div>
+        <img src="./assets/header.jpg" alt="Header Image" class="header-image"/>
+        <Searchbar />
+    </div>
+    <div>
+        <Navbar />
+        <News/>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
