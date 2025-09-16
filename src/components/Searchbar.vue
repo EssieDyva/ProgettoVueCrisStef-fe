74c5509a4070bbc0f670f3df129a1474c202bafa
EssieDyva <st3f1703@gmail.com>
2025-09-16 11:47:29 +0200

    preparing body

--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -15,12 +15,12 @@
 
 <style>
 form {
-    display: flex; 
+    display: flex;
     justify-content: flex-end;
     border-radius: 40px;
     align-items: center;
     background: transparent;
-    border: 3px solid #1800ad; 
+    border: 3px solid #1800ad;
     padding: 8px 15px;
     transition: all 0.3s ease;
     width: 400px;
@@ -28,26 +28,26 @@ form {
 }
 
 .searchbar {
-    display: flex; 
+    display: flex;
     float: right;
 }
 
 ::placeholder {
-    color: #f0e9e9; 
+    color: #f0e9e9;
 }
 
 button {
-    background-color: #211295; 
-    border-radius: 50px; 
+    background-color: #211295;
+    border-radius: 50px;
 }
 
 input {
-    background: transparent; 
-    border: none; 
-    outline: none; 
-    color: #f0e9e9; 
-    font-size: 16px; 
-    width: 200px; 
-    transition: width 0.3s ease; 
+    background: transparent;
+    border: none;
+    outline: none;
+    color: #f0e9e9;
+    font-size: 16px;
+    width: 200px;
+    transition: width 0.3s ease;
 }
 </style>
