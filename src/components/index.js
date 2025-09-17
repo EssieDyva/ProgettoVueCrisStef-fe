d24b7c79064b387faea7e47de7154a5be1779f43
EssieDyva <st3f1703@gmail.com>
2025-09-17 10:50:16 +0200

    fixed wrong imports

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Searchbar} from './Searchbar.vue'
 export { default as News } from './News.vue';
 export { default as Header} from './Header.vue';
 export { default as Body} from './Body.vue';    
+export { default as Map} from './Map.vue'
--- src/main.js
+++ src/main.js
@@ -2,12 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { router } from './router'
-
-
 import 'leaflet/dist/leaflet.css'
-
-
 import L from 'leaflet'
+
 delete L.Icon.Default.prototype._getIconUrl
 L.Icon.Default.mergeOptions({
     iconRetinaUrl: import.meta.url + '/../node_modules/leaflet/dist/images/marker-icon-2x.png',
